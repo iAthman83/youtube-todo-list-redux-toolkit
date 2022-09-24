ee0113026368049cbd1ee248788961f5f42d438e
Mona Lisa <athman.abu@gmail.com>
2022-09-24 15:15:34 +0300

    initial setup

---
+++ src/component/title/TitleComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./title.css";
+
+const TitleComponent = () => {
+  return (
+    <div className="title">
+      <p className="title-text">Create Your Todo List</p>
+    </div>
+  );
+};
+
+export default TitleComponent;
--- src/store/features/todolist/TodoList.jsx
+++ src/store/features/todolist/TodoList.jsx
@@ -3,6 +3,7 @@ import "./todolist.css";
 import SingleTodo from "../singleTodo/SingleTodo";
 import AddTodoForm from "../addTodoForm/AddTodoForm";
 import DeleteButton from "../../../component/deleteTodos/DeleteButton";
+import TitleComponent from "../../../component/title/TitleComponent";
 
 import { useSelector } from "react-redux";
 
@@ -11,7 +12,7 @@ const TodoList = () => {
   console.log(todos);
   return (
     <div className="todoList">
-      <h1>Add A Your Todo</h1>
+      <TitleComponent />
       <AddTodoForm />
       <div className="todoList-ul">
         <SingleTodo />
